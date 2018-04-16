d59132c13bccdf48cd1e724521e0d4975eb40c99
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2018-04-16 19:28:42 +0400

    --

--- src/mocks/sqlite/index.ts
+++ src/mocks/sqlite/index.ts
@@ -1,105 +1,65 @@
-import { SQLite } from '@ionic-native/sqlite';
+import { SQLite, SQLiteDatabaseConfig } from '@ionic-native/sqlite';
 
-export interface SQLiteDatabaseConfig {
-    /**
-     * Name of the database. Example: 'my.db'
-     */
-    name: string;
-    /**
-     * Location of the database. Example: 'default'
-     */
-    location?: string;
-    /**
-     * iOS Database Location. Example: 'Library'
-     */
-    iosDatabaseLocation?: string;
-}
+declare var SQL;
 
-export declare class SQLiteObject {
+export class SQLiteObject {
     _objectInstance: any;
-    constructor(_objectInstance: any);
-    databaseFeatures: {
-        isSQLitePluginDatabase: boolean;
-    };
-    openDBs: any;
-    addTransaction(transaction: (tx: SQLiteTransaction) => void): void;
-    /**
-     * @param fn {any}
-     * @returns {Promise<any>}
-     */
-    transaction(fn: any): Promise<any>;
-    /**
-     * @param fn {Function}
-     * @returns {Promise<any>}
-     */
-    readTransaction(fn: (tx: SQLiteTransaction) => void): Promise<any>;
-    startNextTransaction(): void;
-    /**
-     * @returns {Promise<any>}
-     */
-    open(): Promise<any>;
-    /**
-     * @returns {Promise<any>}
-     */
-    close(): Promise<any>;
-    /**
-     * Execute SQL on the opened database. Note, you must call `create` first, and
-     * ensure it resolved and successfully opened the database.
-     */
-    executeSql(statement: string, params: any): Promise<any>;
-    /**
-     * @param sqlStatements {Array<string | string[]>}
-     * @returns {Promise<any>}
-     */
-    sqlBatch(sqlStatements: Array<string | string[]>): Promise<any>;
-    abortallPendingTransactions(): void;
-}
 
-export interface SQLiteTransaction {
-    start: () => void;
-    executeSql: (sql: any, values: any, success: Function, error: Function) => void;
-    addStatement: (sql: any, values: any, success: Function, error: Function) => void;
-    handleStatementSuccess: (handler: Function, response: any) => void;
-    handleStatementFailure: (handler: Function, response: any) => void;
-    run: () => void;
-    abort: (txFailure: any) => void;
-    finish: () => void;
-    abortFromQ: (sqlerror: any) => void;
-}
+    constructor(_objectInstance: any) {
+        this._objectInstance = _objectInstance;
+    };
 
-export class SQLiteMock extends SQLite {
-    /**
-     * Open or create a SQLite database file.
-     *
-     * See the plugin docs for an explanation of all options: https://github.com/litehelpers/Cordova-sqlite-storage#opening-a-database
-     *
-     * @param config {SQLiteDatabaseConfig} database configuration
-     * @return Promise<SQLiteObject>
-     */
-    create(config: SQLiteDatabaseConfig): Promise<SQLiteObject> {
-        let theResult: SQLiteObject;
+    executeSql(statement: string, params: any): Promise<any> {
+
         return new Promise((resolve, reject) => {
-            resolve(theResult);
-        });
-    };
-    /**
-     * Verify that both the Javascript and native part of this plugin are installed in your application
-     * @returns {Promise<any>}
-     */
-    echoTest(): Promise<any> {
-        return new Promise((resolve, reject) => {
-            resolve();
-        });
-    };
-    /**
-     * Deletes a database
-     * @param config {SQLiteDatabaseConfig} database configuration
-     * @returns {Promise<any>}
-     */
-    deleteDatabase (config: SQLiteDatabaseConfig): Promise<any> {
-        return new Promise((resolve, reject) => {
-            resolve();
+            try {
+                var st = this._objectInstance.prepare(statement, params);
+                var rows: Array<any> = [];
+                while (st.step()) {
+                    var row = st.getAsObject();
+                    rows.push(row);
+                }
+                var payload = {
+                    rows: {
+                        item: function (i) {
+                            return rows[i];
+                        },
+                        length: rows.length
+                    },
+                    rowsAffected: this._objectInstance.getRowsModified() || 0,
+                    insertId: this._objectInstance.insertId || void 0
+                };
+
+                //save database after each sql query 
+
+                var arr: ArrayBuffer = this._objectInstance.export();
+                localStorage.setItem("database", String(arr));
+                resolve(payload);
+            } catch (e) {
+                reject(e);
+            }
         });
     };
 
 }
+
+export class SQLiteMock {
+    public create(config: SQLiteDatabaseConfig): Promise<SQLiteObject> {
+
+        var db;
+        var storeddb = localStorage.getItem("database");
+        // var storeddb = null;
+
+        if (storeddb) {
+            var arr = storeddb.split(',');
+            db = new SQL.Database(arr);
+        }
+        else {
+            db = new SQL.Database();
+        }
+
+        return new Promise((resolve, reject) => {
+            resolve(new SQLiteObject(db));
+        });
+    }
+} 
